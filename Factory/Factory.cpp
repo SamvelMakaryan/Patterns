672a83275809f8b8b9dbebfc05ef498437bfb198
SamvelMakaryan <123547362+SamvelMakaryan@users.noreply.github.com>
2023-03-03 17:58:02 +0400

    Add files via upload

--- Factory/Factory.cpp
+++ Factory/Factory.cpp
@@ -1,1 +1,43 @@
+class Product {
+public:
+    virtual ~Product() = default;
+    virtual void someOperation() = 0;
+};
 
+class concreteProduct1 : public Product {
+public:
+    void someOperation() override {
+
+    }
+};
+
+class concreteProduct2 : public Product {
+public:
+    void someOperation() override {
+
+    }
+};
+
+class Creator {
+public:
+    virtual ~Creator() = default;
+    virtual Creator* FactoryMethod() const = 0;
+};
+
+class Product1Creator : public Creator {
+public:
+    Creator* FactoryMethod() const override {
+        return new Product1Creator;
+    }
+};
+
+class Product2Creator : public Creator {
+public:
+    Creator* FactoryMethod() const override {
+        return new Product2Creator;
+    }
+};
+
+int main() {
+    Product* ptr = 
+}
