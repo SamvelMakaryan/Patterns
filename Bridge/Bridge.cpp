4b1ba5b62686e677b22dad22ac1a84d1e517c3ef
SamvelMakaryan <123547362+SamvelMakaryan@users.noreply.github.com>
2023-03-05 19:24:54 +0400

    Add files via upload

--- Bridge/Bridge.cpp
+++ Bridge/Bridge.cpp
@@ -1,1 +1,47 @@
+class Implementation {
+public:
+    virtual ~Implementation() = default;
+public:
+    virtual void operation() = 0;
+};
 
+class Implementation1 : public Implementation {
+public:
+    void operation() override {
+
+    }
+};
+
+class Implementation2 : public Implementation {
+public:
+    void operation() override {
+
+    }
+};
+
+class Abstraction {
+public:
+    Abstraction(Implementation* imp) : imp(imp) {}
+    virtual ~Abstraction() = default;
+public:
+    virtual void someOperation() = 0;
+protected:
+    Implementation* imp;
+};
+
+class OtherAbstraction1 : public Abstraction {
+public:
+    void someOperation() override {
+        imp->operation();
+    }
+};
+
+class OtherAbstraction2 : public Abstraction {
+public:
+    void someOperation() override {
+        imp->operation();
+    }
+    void otherOperation() {
+
+    }
+};
