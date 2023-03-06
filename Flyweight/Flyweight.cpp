d826da23b7b52949d8fd242f0299e8d5334d43f3
SamvelMakaryan <123547362+SamvelMakaryan@users.noreply.github.com>
2023-03-07 01:51:30 +0400

    Add files via upload

--- Flyweight/Flyweight.cpp
+++ Flyweight/Flyweight.cpp
@@ -1,1 +1,36 @@
+#include <string>
 
+class SharedResurse {
+public:
+    SharedResurse(int field1, const std::string& field2) : field1(field1), field2(field2) {}
+public:
+    std::string someOperation() const {
+        return "some info";
+    }
+private:
+    int field1;
+    std::string field2;
+};
+
+class UniqueResurse {
+public:
+    UniqueResurse(double field1, const std::string& field2) : field1(field1), field2(field2) {}
+public:
+    std::string someOperation() const {
+        return "other info";
+    }
+private:
+    double field1;
+    std::string field2;
+};
+
+class Flyweight {
+public:
+    Flyweight(SharedResurse* resurse) : shared(resurse) {}  
+public:
+    std::string someOperation(const UniqueResurse& unique) const {
+        return shared->someOperation() + " + " + unique.someOperation();
+    }
+private:
+    SharedResurse* shared;
+};
