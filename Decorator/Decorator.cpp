f728580d2b907150f4b603a8cf500e8277294003
SamvelMakaryan <123547362+SamvelMakaryan@users.noreply.github.com>
2023-03-07 00:47:16 +0400

    Add files via upload

--- Decorator/Decorator.cpp
+++ Decorator/Decorator.cpp
@@ -1,1 +1,46 @@
+#include <string>
+
+class Component {
+public:
+    virtual ~Component() = default;
+public:
+    virtual std::string someOperation() const = 0;
+};
+
+class concreteComponent : public Component {
+public:
+    std::string someOperation() const override {
+        return "concreteComponent";
+    }
+};
+
+class Decorator : public Component {
+public:
+    Decorator(Component* component) : component(component) {}
+public:
+    std::string someOperation() const override {
+        return "Decorator + " + component->someOperation();
+    }
+protected:
+    Component* component;
+};
+
+class concreteDecorator1 : public Decorator {
+public:
+    concreteDecorator1(Component* component) : Decorator(component) {}
+public:
+    std::string someOperation() const override {
+        return "concreteDecorator1 + " + Decorator::component->someOperation();
+    }
+};
+
+class concreteDecorator2 : public Decorator {
+public:
+    concreteDecorator2(Component* component) : Decorator(component) {}
+public:
+    std::string someOperation() const override{
+        return "concreteDecorator2 + " + Decorator::component->someOperation();
+    }
+};
+
 
