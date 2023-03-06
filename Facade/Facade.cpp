e243c18a54b7a76831a325ac02a01c78ac6939e4
SamvelMakaryan <123547362+SamvelMakaryan@users.noreply.github.com>
2023-03-07 01:14:24 +0400

    Add files via upload

--- Facade/Facade.cpp
+++ Facade/Facade.cpp
@@ -1,1 +1,39 @@
+#include <string>
+
+class Subsystem1 {
+public:
+    std::string operation1() const {
+        return "Sub1::operation_1 ";
+    }
+    std::string operation2() const {
+        return "Sub1::operation_2 ";
+    }
+};
+
+class Subsystem2 {
+public:
+    std::string operation1() const {
+        return "Sub2::operation_1 ";
+    }
+    std::string operation2() const {
+        return "Sub2::operation_2 ";
+    }
+};
+
+class Facade {
+public:
+    Facade(Subsystem1* sub1, Subsystem2* sub2) : sub1(sub1), sub2(sub2) {}
+public:
+    std::string someOpreations() {
+        std::string result;
+        result += sub1->operation1();
+        result += sub1->operation2();
+        result += sub2->operation1();
+        result += sub2->operation2();
+        return result;
+    }
+protected:
+    Subsystem1* sub1;
+    Subsystem2* sub2;
+};
 
