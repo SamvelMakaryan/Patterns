a79512b412e8ea52634fb523d7794bab5b5a706b
SamvelMakaryan <123547362+SamvelMakaryan@users.noreply.github.com>
2023-03-08 01:38:06 +0400

    Add files via upload

--- Proxy/Proxy.cpp
+++ Proxy/Proxy.cpp
@@ -1,1 +1,29 @@
+class Subject {
+public:
+    virtual ~Subject() = default;
+public:
+    virtual void someOperation() const = 0; 
+};
 
+class RealSubject : public Subject {
+public:
+    void someOperation() const override {
+
+    }
+};
+
+class Proxy : public Subject {
+public:
+    Proxy(RealSubject* real) : realSub(real) {}
+public:
+    void someOperation() const override {
+        otherOperation();
+        realSub->someOperation();
+
+    }
+    void otherOperation() const {
+
+    }
+private:
+    RealSubject* realSub;
+};
